e3fb3e9cc7b547aa059217c96e3e3fd569d82f97
Kvartalnyy.A <andrejkvartalnyj472@gmail.com>
2025-05-13 14:48:18 +0300

    new brunch new tests nwe

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,20 @@
-from src.Classes import Product,Category
+import pytest
 
-if __name__ == '__main__':
+from src.Classes import Category, Product
+
+
+@pytest.fixture()
+def product():
+    return Product("Iphone 16", "512GB, Gray space", 250000.0, 7)
+
+
+@pytest.fixture()
+def category():
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    print(str(product1))
-    print(str(product2))
-    print(str(product3))
-
-    category1 = Category(
+    return Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3]
+        [product1, product2, product3],
     )
-
-    print(str(category1))
-
-    print(category1.products)
-
-    print(product1 + product2)
-    print(product1 + product3)
-    print(product2 + product3)
